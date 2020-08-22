5b46fddd6359fe513f13d4a7fe3b879fdf2f0a00
raziel rodrigues <raziel@upoutsourcing.com>
2020-08-22 00:03:36 -0300

    erros de codigos que concertei

--- assets/main.js
+++ assets/main.js
@@ -1,7 +1,14 @@
-$( document ).ready(function() {
-    var info = document.getElementById('teste-template').innerHTML;
-    console.log(info);
-    var compiled = Handlebars.compile(info);
-    // document.getElementById('#content').innerHTML = compiled({doesWhat: 'fodac'});    
-    $('#content').html(compiled({doesWhat: 'ok com o jequery funciona ne?'}));
-});
+// $( document ).ready(function() {
+//     var info = document.getElementById('teste-template').innerHTML;
+//     console.log(info);
+//     var compiled = Handlebars.compile(info);
+//     document.getElementById('#content').innerHTML = compiled({doesWhat: 'fodac'});    
+//     // $('#content').html(compiled({doesWhat: 'ok com o jequery funciona ne?'}));
+// });
+
+var info = document.getElementById('teste-template').innerHTML;
+console.log(info);
+var compiled = Handlebars.compile(info);
+var x = document.getElementById('content');
+console.log(x);
+x.innerHTML = compiled({doesWhat: 'Good job!'});
