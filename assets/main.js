d4dfb7b11327749d726fa173ffa9592af9bce89f
raziel rodrigues <raziel@upoutsourcing.com>
2020-10-17 22:57:34 -0300

    commit

--- assets/main.js
+++ assets/main.js
@@ -5,13 +5,11 @@
 //         HelloWorld: 'Hello World!'
 //     }));
 // });
+// const url = 'https://gateway.marvel.com:443/v1/public/comics?apikey=6d0cf4d29f7366f7f15d8f9aa401bfe9';
 
 const template = document.getElementById('handlebars-template').innerHTML;
 const compiledTemplate = Handlebars.compile(template);
 const contentInject = document.getElementById('content-inject');
-
-const url = 'https://gateway.marvel.com:443/v1/public/comics?apikey=6d0cf4d29f7366f7f15d8f9aa401bfe9';
-
 
 contentInject.innerHTML = compiledTemplate({
   Users: [
