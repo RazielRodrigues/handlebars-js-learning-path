ea4404caf343db31ef77123345e95767ee5e2f5c
raziel rodrigues <raziel@upoutsourcing.com>
2020-10-18 01:27:43 -0300

    now with ajax

--- assets/main.js
+++ assets/main.js
@@ -7,8 +7,24 @@
 // });
 // const url = 'https://gateway.marvel.com:443/v1/public/comics?apikey=6d0cf4d29f7366f7f15d8f9aa401bfe9';
 
-const template = document.getElementById('handlebars-template').innerHTML;
-const compiledTemplate = Handlebars.compile(template);
-const contentInject = document.getElementById('content-inject');
+$(document).ready(function() {
+    const template = $('#handlebars-template').html();
+    const compiledTemplate = Handlebars.compile(template);
+    $.ajax("data/profiles.json").done(function(data) {
+      $('#content-inject').html(compiledTemplate(data));
+    });
+});
 
-contentInject.innerHTML = compiledTemplate({...});
+// const template = document.getElementById('handlebars-template').innerHTML;
+// const compiledTemplate = Handlebars.compile(template);
+
+//   var xhttp = new XMLHttpRequest();
+//   xhttp.onreadystatechange = function() {
+//     if (this.readyState == 4 && this.status == 200) {
+//       var data = this.responseText;
+//       const contentInject = document.getElementById('content-inject');
+//       contentInject.innerHTML = compiledTemplate(data);
+//     }
+//   };
+//   xhttp.open("GET", "data/profiles.json", true);
+//   xhttp.send();
