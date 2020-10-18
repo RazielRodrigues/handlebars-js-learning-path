9e7e72d34cc16249227929ad88d2b56ae02c8520
raziel rodrigues <raziel@upoutsourcing.com>
2020-10-18 01:28:19 -0300

    notes

--- assets/main.js
+++ assets/main.js
@@ -1,23 +1,14 @@
-// $(document).ready(function() {
-//     const template = $('#handlebars-template').html();
-//     const compiledTemplate = Handlebars.compile(template);
-//     $('#content-inject').html(compiledTemplate({
-//         HelloWorld: 'Hello World!'
-//     }));
-// });
-// const url = 'https://gateway.marvel.com:443/v1/public/comics?apikey=6d0cf4d29f7366f7f15d8f9aa401bfe9';
-
 $(document).ready(function() {
-    const template = $('#handlebars-template').html();
-    const compiledTemplate = Handlebars.compile(template);
-    $.ajax("data/profiles.json").done(function(data) {
-      $('#content-inject').html(compiledTemplate(data));
-    });
+  const template = $('#handlebars-template').html();
+  const compiledTemplate = Handlebars.compile(template);
+  $.ajax("data/profiles.json").done(function(data) {
+    $('#content-inject').html(compiledTemplate(data));
+  });
 });
 
+//TODO: DISCOVER WHY IS NOT WORKING WITH PURE JS
 // const template = document.getElementById('handlebars-template').innerHTML;
 // const compiledTemplate = Handlebars.compile(template);
-
 //   var xhttp = new XMLHttpRequest();
 //   xhttp.onreadystatechange = function() {
 //     if (this.readyState == 4 && this.status == 200) {
